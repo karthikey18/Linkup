e0dae41b30a0f9318b1f93ad5240862b48f23a2d
Karthikey18 <ckarthikeya.chillarigil@gmail.com>
2025-09-25 20:52:50 +0530

    Fix port and add requirements.txt

--- app.py
+++ app.py
@@ -239,6 +239,10 @@ def about(username):
     conn.close()
     return render_template('about.html', username=username, avatar=user[0], bio=user[1])
 
+import os
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
 
