efb4781cd7297665fc54f5eac6e4c814daab6ad0
Karthikey18 <ckarthikeya.chillarigil@gmail.com>
2025-09-25 21:44:40 +0530

    Fix port binding for Render

--- app.py
+++ app.py
@@ -243,7 +243,7 @@ import os
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 10000))
-    app.run(host='0.0.0.0', port=port)git add app.py
+    app.run(host='0.0.0.0', port=port)
 
 
 
